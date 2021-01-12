846207bff8e4d99fdbd96ebd7113071cfb98d315
Leonardo Urbano <urbanstyle_87@hotmail.it>
2021-01-12 19:20:18 +0000

    Implemented full script logic

--- file_organizer/organizer.py
+++ file_organizer/organizer.py
@@ -21,8 +21,20 @@ class Organizer:
         directory = input("Please type the directory you want to sort: ")
         file_in_dir = os.listdir(directory)
         for file in file_in_dir:
+            file_path = os.path.join(directory, file)
             file_ext = os.path.splitext(file)[-1]
             if file_ext in list(FileExtensions().file_types().keys()):
                 folder = FileExtensions().file_types()[file_ext]
-                # TODO: Implement logic to check if the folder exist
-                # TODO: Implement logic to move files
+                folder_path = os.path.join(directory, folder)
+                if not os.path.isdir(folder_path):
+                    os.makedirs(folder_path)
+                new_file_path = os.path.join(folder_path, file)
+                os.replace(file_path, new_file_path)
+                print(f"File {file} moved to folder {folder}\n")
+            else:
+                unrecognized_files = os.path.join(directory, "Miscellaneous")
+                if not os.path.isdir(unrecognized_files):
+                    os.makedirs(unrecognized_files)
+                new_file_path = os.path.join(unrecognized_files, file)
+                os.replace(file_path, new_file_path)
+                print(f"File {file} moved to generic folder Miscellaneous\n")
