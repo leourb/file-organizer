773fe60a7152aef8e788bb5f3bfabdc99626d0d3
Leonardo Urbano <urbanstyle_87@hotmail.it>
2021-01-13 10:17:45 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="file_organizer",
+    version="1.0.0",
+    author="Leonardo Urbano",
+    author_email="leonardo.urbano87@libero.it",
+    packages=setuptools.find_packages(),
+    description="Organizes the files in a folder following the same structure of FileInfo.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    url="https://github.com/leourb/file-organizer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
